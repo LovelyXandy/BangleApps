26d128cec0fce8c8fb51106f56b43447488088e2
LovelyXandy <60007333+LovelyXandy@users.noreply.github.com>
2025-05-22 05:33:23 +0100

    update widget.js

--- apps/widclk/widget.js
+++ apps/widclk/widget.js
@@ -1,13 +1,13 @@
 /* Simple clock that appears in the widget bar. We update once per minute, but don't bother stopping
 if the */
-WIDGETS["wdclk"]={area:"tl",width:52/* g.stringWidth("00:00") */,draw:function() {
+WIDGETS["wdclk"]={area:"tl",width:104/* g.stringWidth("00:00") */,draw:function() {
   if (!this.width) return; // if not visible, return
 g.reset().setFontCustom(atob("AAAAAAAAAAIAAAQCAQAAAd0BgMBdwAAAAAAAdwAB0RiMRcAAAERiMRdwAcAQCAQdwAcERiMRBwAd0RiMRBwAAEAgEAdwAd0RiMRdwAcERiMRdwAFAAd0QiEQdwAdwRCIRBwAd0BgMBAAABwRCIRdwAd0RiMRAAAd0QiEQAAAAAAAAAA="), 32, atob("BgAAAAAAAAAAAAAAAAYCAAYGBgYGBgYGBgYCAAAAAAAABgYGBgYG"), 512+9).setFontAlign(0,0);
   var time = require("locale").time(new Date(),1);
   const now = Date();
   month = now.getMonth() + 1;
   day = now.getDate();
-  g.clearRect(this.x, this.y, this.x+this.width-1, this.y+23).drawString(time+ " "+, this.x+this.width/2, this.y+12); // 5 * 6*2 = 60
+  g.clearRect(this.x, this.y, this.x+this.width-1, this.y+23).drawString(time+ " "+month+'-'+day, this.x+this.width/2, this.y+12); // 5 * 6*2 = 60
   // queue draw in one minute
   if (this.drawTimeout) clearTimeout(this.drawTimeout);
   this.drawTimeout = setTimeout(()=>{
