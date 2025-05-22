5a033581c32e5ffc7449de8793003c84cfdfa6be
LovelyXandy <60007333+LovelyXandy@users.noreply.github.com>
2025-05-22 05:32:20 +0100

    update widget.js

--- apps/widclk/widget.js
+++ apps/widclk/widget.js
@@ -4,7 +4,10 @@ WIDGETS["wdclk"]={area:"tl",width:52/* g.stringWidth("00:00") */,draw:function()
   if (!this.width) return; // if not visible, return
 g.reset().setFontCustom(atob("AAAAAAAAAAIAAAQCAQAAAd0BgMBdwAAAAAAAdwAB0RiMRcAAAERiMRdwAcAQCAQdwAcERiMRBwAd0RiMRBwAAEAgEAdwAd0RiMRdwAcERiMRdwAFAAd0QiEQdwAdwRCIRBwAd0BgMBAAABwRCIRdwAd0RiMRAAAd0QiEQAAAAAAAAAA="), 32, atob("BgAAAAAAAAAAAAAAAAYCAAYGBgYGBgYGBgYCAAAAAAAABgYGBgYG"), 512+9).setFontAlign(0,0);
   var time = require("locale").time(new Date(),1);
-  g.clearRect(this.x, this.y, this.x+this.width-1, this.y+23).drawString(time, this.x+this.width/2, this.y+12); // 5 * 6*2 = 60
+  const now = Date();
+  month = now.getMonth() + 1;
+  day = now.getDate();
+  g.clearRect(this.x, this.y, this.x+this.width-1, this.y+23).drawString(time+ " "+, this.x+this.width/2, this.y+12); // 5 * 6*2 = 60
   // queue draw in one minute
   if (this.drawTimeout) clearTimeout(this.drawTimeout);
   this.drawTimeout = setTimeout(()=>{
